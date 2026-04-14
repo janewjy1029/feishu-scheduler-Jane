1ce4a08add090ece0b366119161d9156e84352f1
janewjy1029 <weijiayingjane@gmail.com>
2026-04-15 00:44:45 +0800

    Create morning-top3.js

---
+++ api/morning-top3.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  await triggerPush('top3');
+  res.json({ success: true });
+}
+
+async function triggerPush(type) {
+  await fetch('https://j2cmgf9cpf.coze.site/api/schedule-push', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ type }),
+  });
+}
